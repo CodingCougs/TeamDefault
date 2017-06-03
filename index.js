249f97edf917f56034d02b47274f1c3062792c60
haidend <haiden.deaton@gmail.com>
2017-06-03 13:44:21 -0700

    Hello my name is haiden

--- index.js
+++ index.js
@@ -1,47 +1,15 @@
-// This will be our main program driver [Probably we will use botkit.]
+var slack = require('slack');
+var   bot = slack.rtm.client();
+
+// start listening to the slack team associated to the token 
+bot.listen({token:require('./config').token});
+
+// on RTM message event
+bot.message(function(msg){ 
+
+// Call ur function
+console.log(msg.text);
 
 
 
-var Botkit = require('botkit');
-
-
-var controller = Botkit.slackbot({
-    require_delivery: true,
-});
-
-
-var bot = controller.spawn({
-    token:require('./config').token
-});
-
-bot.startRTM(function(err,bot,payload) {
- 
-  if (err) {
- 
-    throw new Error('Could not connect to Slack');
- 
-  }
- 
-});
-
-controller.hears(["Hello","Hi","Hola","Hey","boo"],["direct_message"],function(bot,message) {
-
-    if(message.text.toLowerCase() === "hello"){
-        bot.reply(message, 'Hello there <@'+message.user+'>');
-    }
-    
-    else if(message.text.toLowerCase() === "hi"){
-        bot.reply(message, 'Hi there <@'+message.user+'>');
-    }
-    
-    else if(message.text.toLowerCase() === "hey"){
-        bot.reply(message, 'Hey there <@'+message.user+'>');
-    }
-
-    else if(message.text.toLowerCase() === "hola"){
-        bot.reply(message, 'Hola <@'+message.user+'>');
-    }
-    else {
-            bot.reply(message, 'Hi there <@'+message.user+'>');
-    }
 });
