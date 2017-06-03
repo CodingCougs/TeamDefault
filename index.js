a10fda0d43e8f755589b3e981cbbb7a4f2fd87ee
haidend <haiden.deaton@gmail.com>
2017-06-03 11:39:24 -0700

    added basic functionality

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // This will be our main program driver [Probably we will use botkit.]
+
+
+
+var Botkit = require('botkit');
+
+
+var controller = Botkit.slackbot({
+    require_delivery: true,
+});
+
+controller.hears(["Hello","Hi","Hola","Hey","boo"],["direct_message"],function(bot,message) {
+
+    if(message.text.toLowerCase() === "hello"){
+        bot.reply(message, 'Hello there <@'+message.user+'>');
+    }
+    
+    else if(message.text.toLowerCase() === "hi"){
+        bot.reply(message, 'Hi there <@'+message.user+'>');
+    }
+    
+    else if(message.text.toLowerCase() === "hey"){
+        bot.reply(message, 'Hey there <@'+message.user+'>');
+    }
+
+    else if(message.text.toLowerCase() === "hola"){
+        bot.reply(message, 'Hola <@'+message.user+'>');
+    }
+    else {
+            bot.reply(message, 'Hi there <@'+message.user+'>');
+    }
+});
